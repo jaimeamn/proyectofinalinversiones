64edec32384ef7b6cbbef6f023f8f518008ac491
Jaime Martinez <java9017@hotmail>
2022-04-08 17:43:46 -0500

    actualizacion de proyecto

--- inversiones_frontend/src/js/app.js
+++ inversiones_frontend/src/js/app.js
@@ -1,29 +1,34 @@
-(function(){
-    
-    function getMovements(){
+(function() {
+
+    function getMovements() {
         fetch('http://localhost:5000/api/v1/movimientos')
-        .then(response => response.json())
-        .then(data => renderData("tbody-movimientos",data));
+            .then(response => response.json())
+            .then(data => renderData("tbody-movimientos", data));
+    }
+
+
+    function renderData(target, data) {
+
+        let elementTarget = document.getElementById(target);
+
+        data.data.forEach(element => {
+
+
+            let tbodyConten =
+                `<tr> 
+        <td> ${element.fecha} </td>
+        <td> ${element.hora} </td>
+        <td> ${element.moneda_from}</td>
+        <td> ${element.cantidad_from}</td>
+        <td> ${element.moneda_to}</td>
+        <td> ${element.cantidad_to}</td>
+        </tr>`;
+
+            elementTarget.innerHTML += tbodyConten;
+
+        });
+
     }
 
     getMovements();
-
-    function renderData(target, data){
-        let elementTarget = document.getElementById(target);
-
-    data.data.forEach(element => {
-        let tbodyConten = `<tr> 
-        <td> ${element.fecha} </td>
-        <td> ${element.hora} </td>
-        <td> ${element.moneda_from} </td>
-        <td> ${element.cantidad_from} </td>
-        <td> ${element.moneda_to} </td>
-        <td> ${element.cantidad_to} </td>
-        </tr>`;
-      
-        elementTarget.innerHTML+=tbodyConten;
-
-    });
-
-    }
 })()
